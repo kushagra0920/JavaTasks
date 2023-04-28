2ef28a69963056292c51a3cc72c762d86253c8a0
kushagra0920 <KUSHAGRA0920@GMAIL.COM>
2023-04-28 21:17:32 +0530

    log increase

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,10 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main(String[] args) {
+        logger.info("data set");
         SpringApplication.run(App.class, args);
     }
 
-    private static final Logger logger = LogManager.getLogger(App.class);
 
    }
